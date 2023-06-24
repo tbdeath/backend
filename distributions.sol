76b41cf5efe430cc432e98fcdf2ca959301e728b
Maisha Thasin <thasin.maisha@gmail.com>
2023-06-24 17:07:38 -0400

    added wallet address

--- distributions.sol
+++ distributions.sol
@@ -6,26 +6,28 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // TODO: implement metamask and connect wallets instead of tokens in the smart contract 
 // connect to sqlite database to get data (look into web3 connectors)
+
+//get the number of assets in the database 
+// NumberofAssets();
 contract AssetDistribution {
     struct Beneficiary {
         address beneficiaryAddress;
         uint256 share; // Share in percentage
     }
+    
 
     mapping(uint256 => Beneficiary[]) public beneficiariesByAsset;
 
-    function distributeAssets(
-        uint256 assetId,
+    function distributeAssets( 
         address assetOwner,
         uint256 totalShares
-    ) external {
-        require(beneficiariesByAsset[assetId].length > 0, "No beneficiaries assigned to this asset");
+    );
 
-        for (uint256 i = 0; i < beneficiariesByAsset[assetId].length; i++) {
-            Beneficiary memory beneficiary = beneficiariesByAsset[assetId][i];
-            uint256 amountToSend = (IERC20(assetId).balanceOf(assetOwner) * beneficiary.share) / totalShares;
+        for (uint256 i = 0; i < beneficiariesByAsset[NumberofAssets].length; i++) {
+            Beneficiary memory beneficiary = beneficiariesByAsset[NumberofAssets][i];
+            uint256 amountToSend = (IERC20(NumberofAssets).balanceOf(assetOwner) * beneficiary.share) / totalShares;
 
-            IERC20(assetId).transferFrom(assetOwner, beneficiary.beneficiaryAddress, amountToSend);
+            IERC20(NumberofAssets).transferFrom(assetOwner, beneficiary.beneficiaryAddress, amountToSend);
 
             // Update traditional database}
 
