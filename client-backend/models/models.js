8c3300b7680776d152b8ec5c5cccabf81e39e7c5
Maisha Thasin <thasin.maisha@gmail.com>
2023-06-24 16:55:20 -0400

    add the backend for routing and mongoDB

---
+++ client-backend/models/models.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    name: {
+        required: true,
+        type: String
+    },
+    SIN: {
+        required: true,
+        type: Number
+    },
+    share: {
+        required: true,
+        type: Number
+    }
+})
+
+module.exports = mongoose.model('Data', dataSchema)
