b15dba70886cc84fc811b1c3ccd21c4333529626
Maisha Thasin <thasin.maisha@gmail.com>
2023-06-24 17:09:44 -0400

    f

--- client-backend/models/models.js
+++ client-backend/models/models.js
@@ -12,6 +12,10 @@ const dataSchema = new mongoose.Schema({
     share: {
         required: true,
         type: Number
+    },
+    walletAddress:{
+        reuqired: true,
+        type: String
     }
 })
 
